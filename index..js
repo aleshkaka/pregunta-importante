f7561e90353e2af9546bcdd9b5b57d7af9a2494e
aleshkaka <160438119+aleshkaka@users.noreply.github.com>
2024-02-18 18:12:16 -0500

    Add files via upload

---
+++ index..js
@@ -0,0 +1,8 @@
+const yesBtn = document.querySelector('#yesBtn');
+
+yesBtn.addEventListener('click',function () {
+    alert('Valdrá la pena, lo prometo, por ti todo.')
+});
+
+const noBtn =  document.querySelector('#noBtn');
+
